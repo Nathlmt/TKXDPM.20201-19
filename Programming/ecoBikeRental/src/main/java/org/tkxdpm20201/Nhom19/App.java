08c3ee5026fd89628e66979f1cb49e00ad7e1f4b
lytuan99 <lytuan270899@gmail.com>
2020-11-12 19:51:00 +0700

    fix view fxml

--- Programming/ecoBikeRental/src/main/java/org/tkxdpm20201/Nhom19/App.java
+++ Programming/ecoBikeRental/src/main/java/org/tkxdpm20201/Nhom19/App.java
@@ -1,29 +1,38 @@
 package org.tkxdpm20201.Nhom19;
 
 import javafx.application.Application;
-
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 
 
 public class App extends Application {
-    @Override
-    public void start(Stage stage) {
-
-    }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/home_screen.fxml"));
+        StackPane layout = new StackPane();
+        Button button = new Button();
+        button.setText("ok ");
+        layout.getChildren().add(button);
+//        System.out.println("ok nha");
+        stage.setScene(new Scene(root, 300, 300));
+        stage.setTitle("Hello world!");
+        stage.show();
+    }
+
+
+
 
 
 }
